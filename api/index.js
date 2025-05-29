c47ede41bcab359865869de5cac5e44ce34492c0
PerseoN15 <andres.nc@tecjerez.edu.mx>
2025-05-29 00:06:08 -0600

    actualizacion

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import authRoutes from '../routes/authRoutes.js';
+import usuarioRoutes from '../routes/usuarioRoutes.js';
+import materiaRoutes from '../routes/materiaRoutes.js';
+import alumnoRoutes from '../routes/alumnoRoutes.js';
+import verificarToken from '../middlewares/verificarToken.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ Conectado a MongoDB Atlas'))
+  .catch((err) => console.error('❌ Error de conexión:', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/usuarios', verificarToken, usuarioRoutes);
+app.use('/api/materias', verificarToken, materiaRoutes);
+app.use('/api/alumnos', verificarToken, alumnoRoutes);
+
+export default app;
